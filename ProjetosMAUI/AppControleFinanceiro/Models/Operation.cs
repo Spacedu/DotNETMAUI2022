43c0cfe1a93b24aae135b894ef07c547af85e844
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-14 14:15:23 -0300

    S10E07 - TransactionPage - Repository - Operation

---
+++ ProjetosMAUI/AppControleFinanceiro/Models/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Models
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+        public OperationType OperationType { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public double Value { get; set; }
+    }
+
+    public enum OperationType
+    {
+        Income,
+        Expenses
+    }
+}
---
+++ ProjetosMAUI/AppControleFinanceiro/Repositories/OperationRepository.cs
@@ -0,0 +1,54 @@
+﻿using AppControleFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Repositories
+{
+    public class OperationRepository
+    {
+        public List<Operation> GetOperations(DateTimeOffset date)
+        {
+            return new List<Operation>
+            {
+                new Operation { 
+                    Id = 1,
+                    OperationType = OperationType.Income,
+                    Name = "Salário", 
+                    Date = new DateTimeOffset(2022, 10, 10, 0,0,0, new TimeSpan(0, -3, 0,0)),                    
+                    Value = 3600                    
+                },
+                new Operation {
+                    Id = 2,
+                    OperationType = OperationType.Expenses,
+                    Name = "Feira no mercado",
+                    Date = new DateTimeOffset(2022, 10, 11, 0,0,0, new TimeSpan(0, -3, 0,0)),
+                    Value = 600
+                },
+                new Operation {
+                    Id = 3,
+                    OperationType = OperationType.Expenses,
+                    Name = "Sapato",
+                    Date = new DateTimeOffset(2022, 10, 21, 0,0,0, new TimeSpan(0, -3, 0,0)),
+                    Value = 200
+                },
+                new Operation {
+                    Id = 4,
+                    OperationType = OperationType.Expenses,
+                    Name = "Lanche",
+                    Date = new DateTimeOffset(2022, 10, 21, 0,0,0, new TimeSpan(0, -3, 0,0)),
+                    Value = 40
+                },
+            };
+        }
+        public void AddOperations(Operation operation)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteOperations(Operation operation) {
+            throw new NotImplementedException();
+        }
+    }
+}
