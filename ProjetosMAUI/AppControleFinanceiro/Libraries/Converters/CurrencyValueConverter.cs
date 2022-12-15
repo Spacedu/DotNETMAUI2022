3ecb31ae8038cbf3ffeaa35861a06f5e261b8793
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-15 11:38:34 -0300

    S10E09 - TransactionPage - Converter para Receita e Despesa

---
+++ ProjetosMAUI/AppControleFinanceiro/Libraries/Converters/CurrencyValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using AppControleFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Converters
+{
+    public class CurrencyValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value ==null) return null;
+
+            Operation operation = (Operation)value;
+            
+
+            if(operation.OperationType == OperationType.Expenses)
+            {
+                return "- " + operation.Value.ToString("C");
+            }
+            return operation.Value.ToString("C");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
