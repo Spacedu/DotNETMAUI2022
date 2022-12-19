273cfa9c75c209d85abe0ee47621cc0dff2d1927
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-12-19 14:43:34 -0300

    S10E12 - AddTransaction - Programando o Fechamento e os campos

--- ProjetosMAUI/AppControleFinanceiro/Pages/AddTransactionPage.xaml.cs
+++ ProjetosMAUI/AppControleFinanceiro/Pages/AddTransactionPage.xaml.cs
@@ -1,3 +1,5 @@
+using AppControleFinanceiro.Models;
+
 namespace AppControleFinanceiro.Pages;
 
 public partial class AddTransactionPage : ContentPage
@@ -10,5 +12,22 @@ public partial class AddTransactionPage : ContentPage
     protected override void OnSizeAllocated(double width, double height)
     {
         Date.WidthRequest = width - 30;
+    }
+
+    private void OnTapped_Close(object sender, TappedEventArgs e)
+    {
+        Navigation.PopModalAsync();
+    }
+
+    private void OnButtonClicked_Save(object sender, EventArgs e)
+    {
+        //TODO - Validar os dados digitados
+        var operation = new Operation
+        {
+            OperationType = (Receita.IsChecked) ? OperationType.Income : OperationType.Expenses,
+            Name = Name.Text,
+            Date = Date.Date,
+            Value = double.Parse(Value.Text)
+        };
     }
 }
